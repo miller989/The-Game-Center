ace2688cb30b45fcc1843eb2848ffc6c50b29ebd
Josh <contact@beaniebot.me>
2019-11-06 11:31:11 -0500

    Update err

--- www/includes/saveCart.php
+++ www/includes/saveCart.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
+//** SQL housekeeping **//
+$servername = "localhost";  
+$username = "root";   
+$password = "";       
+$dbname = "tgcdb";    
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    header(Location: ../index.php?err=100)
+}
 /** ensure user is logged in**/
 if ( isset( $_SESSION['name'] ) ) {
     $name = $_SESSION['name'];
