e4a988caba8f4e5871450a82c1fee2150da47d2c
Josh <contact@beaniebot.me>
2019-11-06 11:36:49 -0500

    Remove mysql from savecart

--- www/includes/saveCart.php
+++ www/includes/saveCart.php
@@ -6,7 +6,7 @@ $username = "root";
 $password = "";       
 $dbname = "tgcdb";    
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+//$conn = mysqli_connect($servername, $username, $password, $dbname);
 //Check connection
 //if (!$conn) {
   //  die("Connection failed: " . mysqli_connect_error());
