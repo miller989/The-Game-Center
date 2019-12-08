57398f8f36ee76c2a26850ef754e823a8c35400f
Jacob Newahrt <36865572+jacobnewhart@users.noreply.github.com>
2019-12-08 17:39:52 -0500

    Add files via upload

---
+++ www/includes/usersidebar.php
@@ -0,0 +1,38 @@
+<div class="panel panel-default sidebar-menu">
+
+    <div class="panel-heading">
+        <h3 class='panel-title' align='center'></h3>
+    </div>
+
+    <div class="panel-body">
+
+        <ul class="nav-pills nav-stacked nav">
+
+            <li class= "active">
+                <a href="#"><i class="fa fa-list"></i> My Orders</a>
+            </li>
+
+            <li>
+                <a href="#"><i class="fa fa-bolt"></i> Pay Offline</a>
+            </li>
+
+            <li>
+                <a href="#"><i class="fa fa-pencil"></i> Edit Account</a>
+            </li>
+
+            <li>
+                <a href="#"><i class="fa fa-user"></i> Change Password</a>
+            </li>
+
+            <li>
+                <a href="#"> <i class="fa fa-trash-o"></i> Delete Account</a>
+            </li>
+
+            <li>
+                <a href="logout.php"> <i class="fa fa-sign-out"></i> Log Out</a>
+            </li>
+
+        </ul>
+
+    </div>
+</div>
