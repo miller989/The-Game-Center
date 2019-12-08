20f08762ee87de9a98cd94e35369cf44f1d8abd3
Jacob Newahrt <36865572+jacobnewhart@users.noreply.github.com>
2019-12-08 18:12:57 -0500

    Add files via upload

--- www/login.php
+++ www/login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-<?php
+	<?php
 include 'includes/err.php';
 /*Check for errors */
 if (isset ($_GET["err"])){
@@ -12,31 +12,83 @@ if (isset ($_GET["err"])){
 }
 ?>
 	<meta charset="utf-8">
-	<title>Login</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/login.css">
-<link rel="stylesheet" href="assets/fonts/font-awsome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-337.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/fonts/font-awsome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-337.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<title>TGC</title>
 </head>
 
+
 <body>
-<?php
-echo $errhtml;
-?>
-    <div class="loginbox">
-		<h1>Login</h1>
-		<form action="auth.php" method="post">
-			<label for="username">
-			</label>
-			<input type="text" name="username" placeholder="Username" id="username" required>
-			<label for="password">
-			</label>
-			<input type="password" name="password" placeholder="Password" id="password" required>
-			<input class="button" type="submit" value="Login">
-			<a href="register/index.php">Don't have an account?</a>
-		</form>
+<br /><br />
+	<div id="content">
+		<div class="container">
+			<div class="col-md-12">
+
+				<ul class="breadcrumb">
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+					<li>Account </li>
+				</ul>
+
+			</div>
+
+			<div class="col-md-12">
+				<div class="box">
+
+					<div class="box-header">
+
+						<center>
+							<h1> Login </h1>
+						</center>
+
+					</div>
+
+					<form action="auth.php" method="post">
+
+						<div class="form-group">
+
+							<label> Username </label>
+
+							<input type="text" name="username" placeholder="Username" id="username" class="form-control"
+								required>
+
+						</div>
+
+						<div class="form-group">
+
+							<label> Password </label>
+							<input type="password" name="password" placeholder="Password" id="password"class="form-control" required>
+							<br />
+						</div>
+
+						<div class="text-center">
+
+							<button type="submit" value="Login" class="btn btn-primary">Login</button>
+
+						</div>
+					</form>
+
+					<center>
+						<a href="register/index.php">Don't have an account?</a>
+					</center>
+					<br /><br />
+
+				</div>
+			</div>
+		</div>
 	</div>
+	
+    <?php
+    include("includes/footer.php");
+    ?>
+
+    <script src="assets/js/jquery-331.min.js"></script>
+    <script src="assets/js/bootstrap-337.min.js"></script>
 </body>
 
 </html>
--- www/register/index.php
+++ www/register/index.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-<?php
-include 'includes/err.php';
+    <?php
+include '../includes/err.php';
 /*Check for errors */
 if (isset ($_GET["err"])){
     $err = $_GET["err"];
@@ -11,46 +11,113 @@ if (isset ($_GET["err"])){
 }else{
 }
 ?>
-<meta charset="utf-8">
-	<title>TGC</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/login.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/login.css">
-<link rel="stylesheet" href="../assets/fonts/font-awsome/css/font-awesome.min.css">
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../assets/fonts/font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="../assets/css/bootstrap-337.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $("#ConfirmPassword").keyup(function(){
-             if ($("#password").val() != $("#ConfirmPassword").val()) {
-                 $("#msg").html("Password do not match").css("color","red");
-              //   document.getElementById("btnSubmit").disabled = true;
-             }else{
-                 $("#msg").html("Password matched").css("color","green");
-                // document.getElementById("btnSubmit").disabled = false;
-            }
-      });
-});
-</script>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>TGC</title>
+    <script>
+        $(document).ready(function () {
+            $("#ConfirmPassword").keyup(function () {
+                if ($("#password").val() != $("#ConfirmPassword").val()) {
+                    $("#msg").html("Password do not match").css("color", "red");
+                    //   document.getElementById("btnSubmit").disabled = true;
+                } else {
+                    $("#msg").html("Password matched").css("color", "green");
+                    // document.getElementById("btnSubmit").disabled = false;
+                }
+            });
+        });
+    </script>
 </head>
 
-<body>
-	<div class="loginbox">
 
-<form action="register.php" method="post" autocomplete="off">
-				<label for="username">
-				</label>
-				<input type="text" name="username" placeholder="Username" id="username" required>
-				<label for="password">
-				</label>
-                <input type="password" name="password" placeholder="Password" id="password" required>
-<label for="Password Confirmation">
-</label>
-<input type="password" id="ConfirmPassword" placeholder="Password Confirmation" required>
-<div id="msg"></div>
-<input class="button" id=btnSubmit type="submit"  value="Register">
-</form>
-</div>
+<body>
+    <br /><br />
+    <div id="content">
+        <div class="container">
+            <div class="col-md-12">
+
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="../index.php">Home</a>
+                    </li>
+                    <li>Account </li>
+                </ul>
+
+            </div>
+
+            <div class="col-md-12">
+                <div class="box">
+
+                    <div class="box-header">
+
+                        <center>
+                            <h1> Register </h1>
+                        </center>
+
+                    </div>
+
+                    <!-- start -->
+                    <form action="register.php" method="post" autocomplete="off">
+
+                        <div class="form-group">
+                            <label> Username </label>
+                            <input type="text" name="username" placeholder="Username" id="username" class="form-control"required>
+                        </div>
+
+                        <div class="form-group">
+                            <label> Password </label>
+                            <input type="password" name="password" placeholder="Password" id="password"class="form-control" required>
+                            
+                        </div>
+                        
+                        <div class="form-group">
+                            <label> Password Confirmation </label>
+                            <input type="password" id="ConfirmPassword" placeholder="Password Confirmation" class="form-control" required>
+                        </div>
+
+                        <div class="text-center">
+                            <input  id=btnSubmit type="submit" value="Register" class="btn btn-primary">
+                        </div>
+                    </form>
+
+                    <center>
+                        <a href="../login.php">Have an account?</a>
+                    </center>
+                    <br /><br />
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include("../includes/footer.php");
+    ?>
+
+    <script src="assets/js/jquery-331.min.js"></script>
+    <script src="assets/js/bootstrap-337.min.js"></script>
 </body>
 
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
